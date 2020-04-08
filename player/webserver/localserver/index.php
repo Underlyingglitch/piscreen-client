a932ab2bbb52a17281e0a595eca596f00749aa3e
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-08 21:06:34 +0200

    Updated index.php

--- player/webserver/localserver/index.php
+++ player/webserver/localserver/index.php
@@ -3,10 +3,26 @@
 $response = null;
 system("ping -c 1 google.com", $response);
 if($response == 0) {
-    // this means you are connected
-    $client_ip = "Some IP";
-    $client_sec_code = "Sec code";
-    include "start.php";
+  //Getting the local IP address of the server
+  $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+  socket_connect($sock, "8.8.8.8", 53);
+  socket_getsockname($sock, $name);
+
+  //Security code
+  //Check if code is present
+  if (file_exists("/home/pi/piscreen-client/dist/data/securitycode")) {
+    //Get code from file and display to page
+    $code = file_get_contents("/home/pi/piscreen-client/dist/data/securitycode");
+  } else {
+    //Generate code and put in file
+    $code = mt_rand(100000, 999999);
+    file_put_contents("/home/pi/piscreen-client/dist/data/securitycode", $code);
+  }
+
+  //Setting page variables
+  $client_ip = $name;
+  $client_sec_code = $code;
+  include "start.php";
 } else {
   include "noconn.php";
 }
