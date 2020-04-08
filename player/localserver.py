947d151c64af7d972bb947403307e4da8ebc7ef0
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-08 09:53:38 +0200

    Updated localserver.py

--- player/localserver.py
+++ player/localserver.py
@@ -1,18 +1,3 @@
-import socket, time, os
+import os
 
-def internet(host="8.8.8.8", port=53, timeout=3):
-  try:
-    socket.setdefaulttimeout(timeout)
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-    return True
-  except socket.error as ex:
-    print(ex)
-    return False
-
-while True:
-    time.sleep(1)
-    if internet():
-        print('Online')
-    else:
-        print('Offline')
-        os.system('chromium-browser --kiosk /home/pi/piscreen-client/dist/pages/noconn.html')
+os.system('php -S 0.0.0.0:1234 -t /home/pi/piscreen-client/player/webserver/localserver')
