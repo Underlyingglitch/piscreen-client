bbe819c6a7158faeb0bb06df917bdf822e719486
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-08 09:55:01 +0200

    Added default data

--- player/webserver/localserver/index.php
+++ player/webserver/localserver/index.php
@@ -4,6 +4,8 @@ $response = null;
 system("ping -c 1 google.com", $response);
 if($response == 0) {
     // this means you are connected
+    $client_ip = "Some IP";
+    $client_sec_code = "Sec code";
     include "start.php";
 } else {
   include "noconn.php";
