7a86f7c9ab05f66741f9b93f22b838427c215368
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-08 09:47:19 +0200

    Updated localserver

--- install.sh
+++ install.sh
@@ -19,13 +19,13 @@ apt-get -y remove apache2
 apt-get -y purge apache2
 
 echo "Creating startup scripts"
-mv dist/scripts/piscreen-client-networkcheck.service /lib/systemd/system/piscreen-client-networkcheck.service
-chmod 644 /lib/systemd/system/piscreen-client-networkcheck.service
-chmod +x /home/pi/piscreen-client/player/networkcheck.py
+mv dist/scripts/piscreen-client-localserver.service /lib/systemd/system/piscreen-client-localserver.service
+chmod 644 /lib/systemd/system/piscreen-client-localserver.service
+chmod +x /home/pi/piscreen-client/player/localserver.py
 
 echo "Reloading startup sequence"
 systemctl daemon-reload
 
 echo "Starting services"
-systemctl enable piscreen-client-networkcheck.service
-systemctl start piscreen-client-networkcheck.service
+systemctl enable piscreen-client-localserver.service
+systemctl start piscreen-client-localserver.service
