a05c1c9cbdcf376ecc0ee4850fffd7dea0c9ecc8
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-08 17:55:52 +0200

    Update index.php

--- player/webserver/localserver/index.php
+++ player/webserver/localserver/index.php
@@ -1,9 +1,10 @@
 <?php
 
+echo "<div style='display: none'>";
 $response = null;
 system("ping -c 1 google.com", $response);
+echo "</div>";
 if($response == 0) {
-    // this means you are connected
     $client_ip = "Some IP";
     $client_sec_code = "Sec code";
     include "start.php";
