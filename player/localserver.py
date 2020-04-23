4207fedd0e5896206d1f11ae7194d2cad54b082a
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-23 17:15:55 +0200

    Relocation

--- player/localserver.py
+++ player/localserver.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('php -S 0.0.0.0:1234 -t /home/pi/piscreen-client/player/webserver/localserver')
+os.system('php -S 0.0.0.0:1234 -t /home/pi/piscreen-client/player/localserver/')
---
+++ player/startapiserver.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('php -S 0.0.0.0:31804 -t /home/pi/piscreen-client/player/api/')
