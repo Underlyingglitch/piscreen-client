c5819f64b2341a4856943a2fdefe83278b8bce7a
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-06-20 20:21:16 +0200

    Updated update.py script

--- dist/piscreen-client/scripts/update.py
+++ dist/piscreen-client/scripts/update.py
@@ -1,14 +1,34 @@
 import requests, json
 
+# Get local server connection
 with open('/var/piscreen-client/data/serverconn.json') as f:
     connection = json.load(f)
-
 f.close()
 
-response = json.load(requests.get('http://{}:31804/player/checkupdate.php'.format(connection['hostname'])).text)
+with open('/var/piscreen-client/data/securitycode') as f:
+    securitycode = f.read()
+f.close()
 
+# Get update check from connected server
+response = json.load(requests.get('http://{}:31804/player/checkupdate.php?code={}'.format(connection['hostname'], securitycode)).text)
+
+# Get current playlist
 with open('/var/piscreen-client/data/serverfiles/current.json') as f:
     data = json.load(f)
 
-if (response['last_updated'] != data['last_updated']):
-    #Update local files with new server files
+    # Check if playlist is up to date
+    if (response['last_updated'] != data['last_updated']):
+        # Update local files with new server files
+        # Get new playlist details
+        details = json.load(requests.get('http://{}:31804/player/getlastplaylist.php?code={}'.format(connection['hostname'], securitycode)).text)
+
+        # Set last updated to new date
+        data['last_updated'] = response['last_updated']
+
+        # Create new local playlist
+        data['slides'] = details['data']
+
+        # Write new data to file
+        f.write(json.dumps(data))
+
+        print(f.read())
