aa4c40e25c0698720c63278b77fee6b2ce24fa0c
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-06-20 12:22:21 +0200

    Updated reboot sequence

--- player/apiserver/server/reboot.php
+++ player/apiserver/server/reboot.php
@@ -3,6 +3,12 @@
 shell_exec(escapeshellcmd('sudo rm -rf /home/pi/.config/chromium/SingletonCookie'));
 shell_exec(escapeshellcmd('sudo rm -rf /home/pi/.config/chromium/SingletonLock'));
 shell_exec(escapeshellcmd('sudo rm -rf /home/pi/.config/chromium/SingletonSocket'));
+
+shell_exec(escapeshellcmd('sudo crontab -l > mycron'));
+shell_exec(escapeshellcmd('sudo echo "* * * * * python /var/piscreen-client/scripts/update.py >/dev/null 2>&1" >> mycron'));
+shell_exec(escapeshellcmd('sudo crontab mycron'));
+shell_exec(escapeshellcmd('sudo rm mycron'));
+
 shell_exec(escapeshellcmd('sudo reboot'));
 
 ?>
