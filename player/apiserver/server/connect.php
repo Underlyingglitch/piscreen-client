54f5935d3a034d0f4f2fd814b5f79b247f2b419f
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-07-13 11:55:06 +0200

    Added error messages

--- player/apiserver/server/connect.php
+++ player/apiserver/server/connect.php
@@ -73,22 +73,115 @@ if ($server->isCode()) {
           <i class="fas fa-check-circle fa-7x icon"></i>
           <h1 class="title">Geslaagd</h1>
           <p class="text">De client heeft verbinding gemaakt met de server: <?php echo $_GET['server']; ?></p>
+          <p class="text">U kunt dit scherm sluiten</p>
         </div>
       </body>
     </html>
     <?php
   } else {
-    //Set response code to 503 Service unavailable
-    http_response_code(503);
-    //Tell the user
-    echo json_encode(array("message" => "Error while connecting"));
+    ?>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>PiScreen - Foutive code</title>
+
+        <link rel="stylesheet" href="/var/www/localserver/vendor/fontawesome/css/all.css">
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="/var/www/localserver/css/roboto.css">
+
+        <style>
+          body {
+            background: #333;
+          }
+
+          .icon {
+            font-size: 300px;
+            color: red;
+          }
+
+          .title {
+            font-family: Roboto;
+            font-size: 90px;
+            color: white;
+          }
+
+          .text {
+            font-family: Roboto;
+            font-size: 30px;
+            color: white;
+          }
+
+          .main {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translateX(-50%) translateY(-50%);
+          }
+        </style>
+      </head>
+      <body>
+        <div class="main" align="center">
+          <i class="fas fa-times-circle fa-7x icon"></i>
+          <h1 class="title">Error</h1>
+          <p class="text">Onbekende fout! Probeer het later opnieuw! U kunt dit scherm sluiten</p>
+        </div>
+      </body>
+    </html>
+    <?php
   }
 
 } else {
-  //Set response code to 503 Service unavailable
-  http_response_code(503);
-  //Tell the user
-  echo json_encode(array("message" => "Incorrect code"));
+  ?>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>PiScreen - Foutive code</title>
+
+      <link rel="stylesheet" href="/var/www/localserver/vendor/fontawesome/css/all.css">
+
+      <!-- Fonts -->
+      <link rel="stylesheet" href="/var/www/localserver/css/roboto.css">
+
+      <style>
+        body {
+          background: #333;
+        }
+
+        .icon {
+          font-size: 300px;
+          color: red;
+        }
+
+        .title {
+          font-family: Roboto;
+          font-size: 90px;
+          color: white;
+        }
+
+        .text {
+          font-family: Roboto;
+          font-size: 30px;
+          color: white;
+        }
+
+        .main {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateX(-50%) translateY(-50%);
+        }
+      </style>
+    </head>
+    <body>
+      <div class="main" align="center">
+        <i class="fas fa-lock fa-7x icon"></i>
+        <h1 class="title">Foutieve code</h1>
+        <p class="text">U heeft een foutieve code ingevoerd. Sluit dit scherm, en probeer het opnieuw!</p>
+      </div>
+    </body>
+  </html>
+  <?php
 }
 
 ?>
